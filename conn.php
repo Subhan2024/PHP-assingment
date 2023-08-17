36e6a12c66c0fd06637a48ed4da20ac69a6c92f8
unknown <sk0251198@gmail.com>
2023-08-18 00:24:29 +0500

    first commit

--- conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost" , "root" , "" , "assingment-2");
-if(!$conn){
-    die("query failed");
-}
-?>
---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect("localhost" , "root" , "" , "assingment");
+if(!$conn){
+    die("query failed");
+}
+?>
